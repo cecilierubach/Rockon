0458d704b9a343574e0a580b0477acbbd26f471b
Cecilie Rubach <cecilierubach@cecilies-macbook-pro.local>
2019-07-12 09:12:08 +0200

    Initial Commit

---
+++ LetsRock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LetsRock
+//
+//  Created by Cecilie Rubach on 12.07.2019.
+//  Copyright © 2019 cecilie.rubach@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
